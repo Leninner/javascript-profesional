91590b24f176ce58cddc21e05989b7ec9a83d98d
leninner <mazabandalenin180@gmail.com>
2021-08-11 19:59:02 -0500

    Update

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,14 @@ import MediaPlayer from "./mediaPlayer.js";
 import autoPlay from "./plugins/autoplay.js";
 
 const video = document.querySelector("video"); // Cuando es el único elemento no es necesario ponerlo con el identificar de selector
-const player = new MediaPlayer({ el: video, plugins: [new autoPlay()] }); // Creando una instancia de la función constructora, le debemos pasar un objeto con la llave (el) y la data que es el (video).
+const player = new MediaPlayer({
+    el: video,
+    plugins: [
+        //new autoPlay()
+    ],
+});
+
+// Creando una instancia de la función constructora, le debemos pasar un objeto con la llave (el) y la data que es el (video).
 //NOTE: (el) es un nombre de llave, lo que significa que si podremos cambiarlo después
 
 const btnPlayPause = document.querySelector("#btnPlayPause"),
